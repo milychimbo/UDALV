442a8d86ee111eebcd773e4cf27286deae876149
milychimbo <68032094+milychimbo@users.noreply.github.com>
2020-07-14 16:33:40 -0500

    Add files via upload

---
+++ UDALV/src/main/java/com/example/UDALV/UdalvApplication.java
@@ -0,0 +1,13 @@
+package com.example.UDALV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdalvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdalvApplication.class, args);
+	}
+
+}
